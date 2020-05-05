188a0e38d0b594c5951d18e1876f7a6104acb457
Jayesh Janyani <jayeshjanyani@Jayeshs-MacBook-Air.local>
2020-05-05 11:22:13 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Navbar,Jumbotron,Container} from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />{'  '}
+      React Navbar
+    </Navbar.Brand>
+  </Navbar>
+  <Jumbotron fluid>
+  <Container>
+    <h1>Fluid jumbotron</h1>
+    <p>
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+  </Jumbotron>
+  
+  </>
   );
 }
 
